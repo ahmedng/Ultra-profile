d974cbd0d1418b727339fd7c8948dd51bf04a085
ahmedng <ahmedng207@gmail.com>
2019-10-01 04:45:03 +0200

    Updates

---
+++ precache-manifest.4998276db758291d00f4654c43d132f4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3d3a8343e5af853ab804bb654128b6",
+    "url": "/Ultra-profile/index.html"
+  },
+  {
+    "revision": "090aa7831ca607210f34",
+    "url": "/Ultra-profile/static/css/main.e4658640.chunk.css"
+  },
+  {
+    "revision": "19b9390dd769d99afde7",
+    "url": "/Ultra-profile/static/js/2.e31bcfac.chunk.js"
+  },
+  {
+    "revision": "090aa7831ca607210f34",
+    "url": "/Ultra-profile/static/js/main.4e526f04.chunk.js"
+  },
+  {
+    "revision": "358324da80093877b996",
+    "url": "/Ultra-profile/static/js/runtime-main.e21245bb.js"
+  },
+  {
+    "revision": "020c9eb834982825b30175d37a0d17d0",
+    "url": "/Ultra-profile/static/media/about-bg.020c9eb8.jpg"
+  },
+  {
+    "revision": "6b1d7b9249e7cdf3d86b567d3032115a",
+    "url": "/Ultra-profile/static/media/home-bg.6b1d7b92.jpg"
+  },
+  {
+    "revision": "e72c2187023a4f81e5beb99fc01e51f9",
+    "url": "/Ultra-profile/static/media/portfolio-img1.e72c2187.jpg"
+  },
+  {
+    "revision": "178ffa234e75dfd850b04b650bd773c8",
+    "url": "/Ultra-profile/static/media/portfolio-img2.178ffa23.jpg"
+  },
+  {
+    "revision": "91f85a2bf75b457555982a3743ca76c3",
+    "url": "/Ultra-profile/static/media/portfolio-img3.91f85a2b.jpg"
+  },
+  {
+    "revision": "03b826c600c300db9d78b22726166a33",
+    "url": "/Ultra-profile/static/media/portfolio-img4.03b826c6.jpg"
+  },
+  {
+    "revision": "08c9320c5e5181f30d939a6dc5b7c6d1",
+    "url": "/Ultra-profile/static/media/portfolio-img5.08c9320c.jpg"
+  },
+  {
+    "revision": "3cd14e36160e75f1e4c2a960bc85423c",
+    "url": "/Ultra-profile/static/media/portfolio-img6.3cd14e36.jpg"
+  },
+  {
+    "revision": "1751e3ccf93843a345010795fc79dcd4",
+    "url": "/Ultra-profile/static/media/portfolio-img7.1751e3cc.jpg"
+  },
+  {
+    "revision": "bd46a53097c1c0cc4d916c2a724c6627",
+    "url": "/Ultra-profile/static/media/portfolio-img8.bd46a530.jpg"
+  }
+]);
